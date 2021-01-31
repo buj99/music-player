5ee54c9cfe3b98def3fa7451e931a10e3a42912d
Bujenita Claudiu Dumitrel <bujenitaclaudiu@gmail.com>
2021-01-31 11:39:25 +0200

    current song highlight

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -3,7 +3,7 @@ import React from "react";
 const LibrarySong = ({
   song,
   songs,
-  setSong,
+  setSongs,
   curentSong,
   setCurentSong,
   audioRef,
@@ -20,18 +20,38 @@ const LibrarySong = ({
       currentTime: audioRef.current.currentTime,
       duration: audioRef.current.duration,
     });
+    //set active
+
+    const newSongs = songs.map((stateSong) => {
+      if (stateSong.id === song.id) {
+        return {
+          ...stateSong,
+          active: true,
+        };
+      } else {
+        return {
+          ...stateSong,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    //check if is playing
     if (isPlaying) {
       const playPromise = audioRef.current.play();
-      if(playPromise!==undefined){
-        playPromise.then(()=>{
+      if (playPromise !== undefined) {
+        playPromise.then(() => {
           audioRef.current.play();
-        })
+        });
       }
     }
   };
   //return
   return (
-    <div onClick={songClick} className="library-song">
+    <div
+      onClick={songClick}
+      className={` library-song ${song.active ? "selected" : ""} `}
+    >
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
